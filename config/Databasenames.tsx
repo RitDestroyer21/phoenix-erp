1d55fffea4662c12f8e49b74620ee7f291f506e1
RitDestroyer21 <rit.chakraborty21@gmail.com>
2026-02-22 22:11:52 +0530

    PhoenixERP Changelog v0.0002 : Database config file added
    Signed-off-by: RitDestroyer21 <rit.chakraborty21@gmail.com>

---
+++ config/Databasenames.tsx
@@ -0,0 +1,9 @@
+export const DatabaseTableNames = {
+  SCHEMA: 'erp',
+  TABLES: {
+    DEPARTMENTS: 'departments',
+    DEGREE:'degree',
+    SEMESTERS:'degreewisesemestermapping',
+    SUBJECTS:'semesterwisesubjectmapping',
+  },
+} as const;
